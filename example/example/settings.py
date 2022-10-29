d1ecdbc2e9410d1c3fe1ec3d8c89255a34a7578e
Mohamed El-Kalioby <mkalioby@mkalioby.com>
2022-10-29 13:27:44 +0300

    Changed Default to Platform authenticator and fixed minor issues in package

--- example/example/settings.py
+++ example/example/settings.py
@@ -132,4 +132,4 @@ AUTHENTICATION_BACKENDS = ['passkeys.backend.PasskeyModelBackend']
 
 FIDO_SERVER_ID="localhost"      # Server rp id for FIDO2, it the full domain of your project
 FIDO_SERVER_NAME="TestApp"
-KEY_ATTACHMENT = passkeys.Attachment.CROSS_PLATFORM
+KEY_ATTACHMENT = passkeys.Attachment.PLATFORM
