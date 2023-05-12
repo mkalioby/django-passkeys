b4ee6fdcc2626beef94cb27c6b9653c6fe9b5538
Gaspar Brogueira <gasparmrb@gmail.com>
2023-05-12 16:03:31 +0100

    Fix template PassKeys and check if passkey is enable

--- passkeys/backend.py
+++ passkeys/backend.py
@@ -1,16 +1,18 @@
 from django.contrib.auth.backends import ModelBackend
-
+from .FIDO2 import auth_complete
 
 class PasskeyModelBackend(ModelBackend):
     def authenticate(self, request, username='',password='', **kwargs):
+
         if request is None:
             raise Exception('request is required for passkeys.backend.PasskeyModelBackend')
+
         if username!='' and password != '':
             request.session["passkey"]={'passkey':False}
             return super().authenticate(request,username=username,password=password, **kwargs)
-        passkeys = request.POST.get('passkeys')
-        if not passkeys:
+
+        passkeys = request.POST.get('passkeys', None)
+        if passkeys is None or passkeys == '':
             raise Exception("Can't find '%s' key in request.POST, did you add the hidden input?")
-        if len(passkeys)>5:
-            from .FIDO2 import auth_complete
-            return auth_complete(request)
+
+        return auth_complete(request)
