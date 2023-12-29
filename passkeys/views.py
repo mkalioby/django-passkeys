ae5559e2d6e435f023a5f5e1923219d0315ebd64
nnsnodnb <nnsnodnb@gmail.com>
2023-12-29 17:57:31 +0900

    Modify syntax

--- passkeys/views.py
+++ passkeys/views.py
@@ -4,19 +4,21 @@ from django.shortcuts import render
 
 from .models import UserPasskey
 
+
 @login_required
-def index(request,enroll=False): # noqa
-    keys = UserPasskey.objects.filter(user=request.user) # pragma: no cover
-    return render(request,'PassKeys.html',{"keys":keys,"enroll":enroll}) # pragma: no cover
+def index(request, enroll=False):  # noqa
+    keys = UserPasskey.objects.filter(user=request.user)  # pragma: no cover
+    return render(request, 'PassKeys.html', {"keys": keys, "enroll": enroll})  # pragma: no cover
 
 
 @login_required
 def delKey(request):
     key=UserPasskey.objects.get(id=request.GET["id"])
-    if key.user.pk  == request.user.pk:
+    if key.user.pk == request.user.pk:
         key.delete()
         return HttpResponse("Deleted Successfully")
     return HttpResponse("Error: You own this token so you can't delete it", status=403)
+
 
 @login_required
 def toggleKey(request):
